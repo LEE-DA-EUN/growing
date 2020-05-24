b17140e80f5e4928868c6194ac74f37e7c79d7d1
danbibibi <dlaeksql08222@gmail.com>
2020-05-24 07:51:54 -0400

    splash

---
+++ sinabro/sinabro/app/src/main/java/com/example/sinabro/singleLineRecord.java
@@ -0,0 +1,28 @@
+package com.example.sinabro;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class singleLineRecord extends Fragment {
+
+    public singleLineRecord() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_single_line_record, container, false);
+    }
+}
