437bc877d496c92ab81130620cf22e52583313ca
danbibibi <dlaeksql08222@gmail.com>
2020-05-31 04:31:49 -0400

    single_line user_input modification

--- sinabro/sinabro/app/src/main/java/com/example/sinabro/SingleLineRecord.java
+++ sinabro/sinabro/app/src/main/java/com/example/sinabro/SingleLineRecord.java
@@ -1,5 +1,4 @@
 package com.example.sinabro;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,28 +8,72 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SingleLineRecord extends Fragment {
 
     public SingleLineRecord() {
         // Required empty public constructor
     }
 
+    ArrayAdapter <String> slistViewAdapter;
+
+    public String getDate(){ // 날짜 구하기
+
+        String strResult = "";
+
+        Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+        strResult += Integer.toString(year) + ".";
+
+        int month = calendar.get(Calendar.MONTH);
+        strResult += Integer.toString(month+1) + ".";
+
+        int date = calendar.get(Calendar.DAY_OF_MONTH);
+        strResult += Integer.toString(date);
+
+        return strResult;
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
-//        EditText single_line_record = (EditText)(R.id.sigle_line_record);
+        View single_line_view = inflater.inflate(R.layout.fragment_single_line_record, container, false);
 
-        return inflater.inflate(R.layout.fragment_single_line_record, container, false);
+        ListView single_line_list = (ListView) single_line_view.findViewById(R.id.stored_list);
+        Button storage_btn = (Button) single_line_view.findViewById(R.id.store);
+        final EditText single_line_record = (EditText) single_line_view.findViewById(R.id.sigle_line_record);
+
+        final ArrayList<String> user_record = new ArrayList<String>();
+
+        storage_btn.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                String user_input_data = getDate() + "  :  " + single_line_record.getText().toString();
+                user_record.add(user_input_data);
+                slistViewAdapter.notifyDataSetChanged();
+            }
+        });
+
+        slistViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_expandable_list_item_1,
+                user_record
+        );
+
+        single_line_list.setAdapter(slistViewAdapter);
+
+
+        return single_line_view;
     }
 
 }
