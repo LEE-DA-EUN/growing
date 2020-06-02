789ba1d1eb11efb596f25754d2532c4497666344
danbibibi <dlaeksql08222@gmail.com>
2020-06-02 10:55:57 -0400

    single line detail activity

---
+++ sinabro/sinabro/app/src/main/java/com/example/sinabro/Detail_singleline.java
@@ -0,0 +1,26 @@
+package com.example.sinabro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class Detail_singleline extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single__line__detail);
+
+        Intent intent = getIntent();
+        TextView today = (TextView) findViewById(R.id.detailed);
+        TextView mtext = (TextView) findViewById(R.id.detail_main_text);
+        today.setText(intent.getStringExtra("record").substring(13));
+        String dayed = intent.getStringExtra("record").substring(0,9);
+        mtext.setText("당신의 " + dayed+ "일의 기록입니다.");
+    }
+}
--- sinabro/sinabro/app/src/main/java/com/example/sinabro/SingleLineRecord.java
+++ sinabro/sinabro/app/src/main/java/com/example/sinabro/SingleLineRecord.java
@@ -1,4 +1,5 @@
 package com.example.sinabro;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,10 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -48,7 +53,7 @@ public class SingleLineRecord extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
-        View single_line_view = inflater.inflate(R.layout.fragment_single_line_record, container, false);
+        final View single_line_view = inflater.inflate(R.layout.fragment_single_line_record, container, false);
 
         ListView single_line_list = (ListView) single_line_view.findViewById(R.id.stored_list);
         Button storage_btn = (Button) single_line_view.findViewById(R.id.store);
@@ -62,6 +67,16 @@ public class SingleLineRecord extends Fragment {
                 user_record.add(user_input_data);
                 slistViewAdapter.notifyDataSetChanged();
             }
+
+        });
+
+        single_line_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String detail_record = slistViewAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), Detail_singleline.class);
+                intent.putExtra("record", detail_record);
+                startActivity(intent);
+            }
         });
 
         slistViewAdapter = new ArrayAdapter<String>(
@@ -69,12 +84,11 @@ public class SingleLineRecord extends Fragment {
                 android.R.layout.simple_expandable_list_item_1,
                 user_record
         );
-
         single_line_list.setAdapter(slistViewAdapter);
-
 
         return single_line_view;
     }
+
 
 }
 
